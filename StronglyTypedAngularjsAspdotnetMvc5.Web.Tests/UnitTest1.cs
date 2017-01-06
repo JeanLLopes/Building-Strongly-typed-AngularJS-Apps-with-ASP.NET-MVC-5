ce61bbde06edcc25b557664d0e26e077fcc985a1
JeanLLopes <jean.llopes@outlook.com>
2017-01-05 23:14:00 -0200

    Add project files.

---
+++ StronglyTypedAngularjsAspdotnetMvc5.Web.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StronglyTypedAngularjsAspdotnetMvc5.Web.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
